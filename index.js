9b54f25d4e33d80d602a8f4c0376aabe2f0157cf
Sebapechi <sebaspechi21@gmail.com>
2023-05-24 11:51:07 -0300

    tercera actualizacion

--- index.js
+++ index.js
@@ -87,6 +87,60 @@ function ingresoproducto() {
     }
 }
 
-function agregandoIva() {
+
+
+let nombresArr = []
+let costArr = []
+let sizeArr = []
+let true0false = []
+
+class productos {
+
+  constructor (name,cost, size) {
+     
+    this.name = name
+    this.cost = cost 
+    this.size = size
+
+
+  }
 
 }
+
+
+function initprogram() {
+    let selectSection = prompt ("¿Que quieres ver? \n 1.Añadir producto \n 2.Escoge el color \n 3.Escoge el talle \n 4.Ver productos \n 5.Añadir el ultimo producto \n 6.Añadir el primer producto \n 7.Buscar producto \n 8.Ver productos con - \n 9.slir")
+
+}
+ while (true0false) {
+      switch(selectSection){
+        case "1":
+            agregarproducto()
+            break;
+
+      }
+ }
+
+
+
+ function agregarproducto(){
+     let name = prompt ("Intriduce el nombre del producto");
+     let cost = Number(prompt("Introduce el costo del producto"));
+     let size = prompt("Introduce el talle del producto")
+     while (true0false){
+        if (name == "" || cost == 0 || isNaN(cost) || size == "" ){
+             alert("No has introducido un valor")
+         let name = prompt ("Intriduce el nombre del producto");
+         let cost = Number(prompt("Introduce el costo del producto"));
+         let size = prompt("Introduce el talle del producto")
+        }
+        else {
+            let productos = new productos (name, cosr, size)
+            productosArr.push (producto)
+            cosole.log (productosArr)
+            initprogram()
+        }
+
+     }
+ }
+
